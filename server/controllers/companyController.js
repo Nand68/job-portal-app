ef860c76a44a2803ba3581935535c581a2d65d3f
Nand68 <nandkishordhadhal2@gmail.com>
2025-01-27 17:42:53 +0530

    Making of User rouuter and controller

--- server/controllers/companyController.js
+++ server/controllers/companyController.js
@@ -87,7 +87,18 @@ export const loginCompany = async (req, res) => {
 };
 
 // Get Company Data
-export const getCompanyData = async (req, res) => {};
+export const getCompanyData = async (req, res) => {
+
+  
+
+  try {
+    const company = req.company
+    res.json({success:true,company})
+  } catch (error) {
+    res.json({success:false,message:error.message})
+  }
+
+};
 
 // Post a new job
 export const postJob = async (req, res) => {
@@ -114,13 +125,46 @@ export const postJob = async (req, res) => {
 };
 
 // Get Company Job applicants
-export const getCompanyJobApplicants = async (req, res) => {};
+export const getCompanyJobApplicants = async (req, res) => {
+
+};
 
 // Get Company Posted Jobs
-export const getCompanyPostedJobs = async (req, res) => {};
+export const getCompanyPostedJobs = async (req, res) => {
+  try {
+    const companyID = req.company._id
+    const jobs = await Job.find({companyID})
+
+    // (Todo adding no of applicants data)
+
+    res.json({success:true,jobsData:jobs})
+
+  } catch (error) {
+    res.json({success:false,message:error.message})
+  }
+};
 
 // Change job application status
-export const ChangeJobApplicationStatus = async (req, res) => {};
+export const ChangeJobApplicationStatus = async (req, res) => {
+
+};
 
 // Change job visibility
-export const ChangeVisiblity = async (req, res) => {};
+export const ChangeVisiblity = async (req, res) => {
+  try {
+    const {id} = req.body
+
+    const companyId = req.body._id
+
+    const job = await Job.findById(id)
+
+    if (companyId.toString() === job.companyId.toString() ) {
+      job.visible = !job.visible
+    }
+
+    await job.save()
+    res.json({success:true,job})
+  } catch (error) {
+    res.json({success:false,message:error.message})
+  }
+};
--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ import { clerkWebhooks } from './controllers/webhooks.js'
 import companyRoutes from './routes/companyRoutes.js'
 import connectCloudinary from './config/cloudinary.js'
 import jobRoutes from './routes/jobRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+import {clerkMiddleware} from '@clerk/express'
 
 // Initialize express 
 const app = express()
@@ -19,7 +21,7 @@ await connectCloudinary()
 // Middlewares
 app.use(cors())
 app.use(express.json())
-
+app.use(clerkMiddleware())
 
 // Routes
 app.get('/',(req,res)=> res.send("API WORKING"))
@@ -29,6 +31,7 @@ app.get("/debug-sentry", function mainHandler(req, res) {
 app.post('/webhooks',clerkWebhooks)
 app.use('/api/company',companyRoutes)
 app.use('/api/jobs',jobRoutes)
+app.use('/api/users',userRoutes)
 
 
 // Port
